8350a7511ae7633c390977f5e8ecb17e433e19a5
Evan Kearney <evanjkearney@gmail.com>
2017-09-08 13:24:27 -0500

    We got the data... We did it

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,6 +13,25 @@ function readyNow () {
             console.log('page load');
         }
     })
+
+    function gameStart(){
+        ourData = {
+            gameMode: $(':selected').val(),
+            firstPlayer: $('#Player1').val(),
+            secondPlayer: $('#Player2').val(),
+            thirdPlayer: $('#Player3').val(),
+            fourthPlayer: $('#Player4').val()
+        }
+
+        $.ajax({
+            method: 'POST',
+            url: '/setup',
+            data: ourData,
+            success: function(res){
+                console.log('New game data sent');
+            }
+        })
+    }
 }
 
 $(document).ready(readyNow);
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var path = require('path');
 var port = 3000;
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(port, function(){
     console.log('server working', port);
@@ -12,4 +14,14 @@ app.listen(port, function(){
 app.get('/', function(req, res){
     res.sendFile(path.resolve('public/views/index.html'));
     console.log('base url');
+})
+
+app.post('/setup', function(req, res){
+    console.log(
+        req.body.gameMode,
+        req.body.firstPlayer,
+        req.body.secondPlayer,
+        req.body.thirdPlayer,
+        req.body.fourthPlayer
+    );
 })
